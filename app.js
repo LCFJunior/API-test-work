876d0188f666f612cd3e90b00d19ea0f5afc0ad0
Luiz Carlos Francisco Junior <129459925+LCFJunior@users.noreply.github.com>
2024-04-08 00:26:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+//API's: New York Times API   |   OpenGraph API
+
+require('dotenv').config();
+const fetch = require('node-fetch');
+const apiKeyNYT = /*your_key*/;
+const urlNYT = `https://api.nytimes.com/svc/archive/v1/2022/3.json?&api-key=${apiKeyNYT}`;
+
+fetch(urlNYT)
+  .then(response => response.json())
+  .then(data => {
+    if (data.response && data.response.docs && data.response.docs.length > 0) {
+      const randomIndex = Math.floor(Math.random() * data.response.docs.length);
+      const randomArticle = data.response.docs[randomIndex];
+
+      const articleUrl = randomArticle.web_url;
+
+      if (!articleUrl || !articleUrl.startsWith('http')) {
+        console.error('Invalid article URL:', articleUrl);
+        return;
+      }
+
+      const openGraphUrl = `https://opengraph.io/api/1.1/site/${encodeURIComponent(articleUrl)}?app_id=${process.env.OPENGRAPH_APP_ID}`;
+
+      fetch(openGraphUrl)
+        .then(response => response.json())
+        .then(data => {
+          if (data.error) {
+            console.log('Error fetching Open Graph information:', data.error.message);
+          } else {
+            console.log('ㅤㅤㅤㅤㅤㅤ-------------------------------------------------------------------------------------------------------------------------------------------------------------------ㅤㅤㅤㅤㅤㅤ')
+            console.log('ㅤ')
+            console.log('ㅤTITLE:', data.hybridGraph.title );
+            console.log('ㅤ')
+            console.log('ㅤDESCRIPTION:', data.hybridGraph.description);
+            console.log('ㅤ')
+            console.log('ㅤIMAGE:', data.hybridGraph.image);
+            console.log('ㅤ')
+            console.log('ㅤPAGE URL:', articleUrl);
+            console.log('ㅤ')
+            console.log('ㅤㅤㅤㅤㅤㅤ-------------------------------------------------------------------------------------------------------------------------------------------------------------------ㅤㅤㅤㅤㅤㅤ')
+          }
+        })
+        .catch(error => console.error('Error fetching Open Graph information:', error));
+    } else {
+      console.log('No documents found.');
+    }
+  })
+  .catch(error => console.error('Error fetching NYT request:', error));
